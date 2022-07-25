c543e33b20eaeb8d00836446d606c986934450d7
Yuanlin Lin <im.yuanlinLin@gmail.com>
2022-07-25 18:16:36 +0800

    add auth callback endpoint

---
+++ pages/auth/callback.tsx
@@ -0,0 +1,19 @@
+import { NextPageContext } from 'next';
+
+export default function () {
+  return <div />;
+}
+
+export function getServerSideProps(context: NextPageContext) {
+  const tokenBase64 = context.query.result;
+  if(tokenBase64 && typeof tokenBase64 === 'string') {
+    const token = Buffer.from(tokenBase64, 'base64').toString('ascii');
+    context.res?.setHeader('Set-Cookie', `token=${token}`);
+  }
+  return {
+    redirect: {
+      permanent: false,
+      destination: '/projects',
+    },
+    props:{},
+  };}
