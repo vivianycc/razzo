52bd08f49df5d2bf4a198e5d4dc75cb537f4ce06
Yuanlin Lin <im.yuanlinLin@gmail.com>
2022-07-25 18:39:38 +0800

    update set cookie domain

--- pages/auth/callback.tsx
+++ pages/auth/callback.tsx
@@ -8,7 +8,10 @@ export function getServerSideProps(context: NextPageContext) {
   const tokenBase64 = context.query.result;
   if(tokenBase64 && typeof tokenBase64 === 'string') {
     const token = Buffer.from(tokenBase64, 'base64').toString('ascii');
-    context.res?.setHeader('Set-Cookie', `token=${token}; path=/`);
+    context.res?.setHeader(
+      'Set-Cookie',
+      `token=${token}; path=/; domain=razzo.app`
+    );
   }
   return {
     redirect: {
