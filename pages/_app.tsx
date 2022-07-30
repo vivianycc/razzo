f2ea847f0bbd1649e740018d3e5279b929680952
Yuanlin Lin <im.yuanlinLin@gmail.com>
2022-07-30 19:00:05 +0800

    sync token from cookie to localstorage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,24 @@
 import '@styles/globals.css';
 import { ApolloProvider } from '@apollo/client';
 import { appWithTranslation } from 'next-i18next';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import client from '@/apollo-client';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({
+  Component,
+  pageProps
+}: AppProps) {
+
+  useEffect(() => {
+    const tokenInCookie = document.cookie.split(';')
+      .find(c => c.trim().startsWith('token='));
+    const tokenInLocalStorage = localStorage.getItem('token');
+    if (tokenInCookie && !tokenInLocalStorage) {
+      localStorage.setItem('token', tokenInCookie.split('=')[1]);
+    }
+  }, []);
+
   return <ApolloProvider client={client}>
     <Component {...pageProps} />
   </ApolloProvider>;
