288cd27efcfc1bff5ab86b7b5fb251a04c770031
Yuanlin Lin <im.yuanlinLin@gmail.com>
2022-07-30 18:49:22 +0800

    split apollo client wsLink and httpLink

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,12 +1,19 @@
-import { ApolloClient, ApolloLink, InMemoryCache } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+import {
+  ApolloClient,
+  ApolloLink,
+  concat,
+  HttpLink,
+  InMemoryCache,
+  split
+} from '@apollo/client';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 const API_URL = 'api.razzo.app/graphql';
 
 function getToken() {
-  if(isBrowser()) {
+  if (isBrowser()) {
     return localStorage.getItem('token') || 'undefined';
   }
   return 'undefined';
@@ -23,19 +30,36 @@ function isBrowser() {
   return typeof window !== 'undefined';
 }
 
-const authLink = setContext((_, { headers }) => {
-  return {
-	  headers: {
-      	...headers,
-      	authorization: `Bearer ${getToken()}`
-	  }
-  };
+const httpLink = new HttpLink({ uri: 'https://' + API_URL, });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: `Bearer ${getToken()}`
+    }
+  }));
+  return forward(operation);
 });
+
+const splitLink = () => {
+  return split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return (
+        definition.kind === 'OperationDefinition' &&
+        definition.operation === 'subscription'
+      );
+    },
+    getWSLink(),
+    concat(authMiddleware, httpLink)
+  );
+};
 
 const client = new ApolloClient({
   uri: 'https://' + API_URL,
   cache: new InMemoryCache(),
-  link: isBrowser() ? ApolloLink.from([authLink, getWSLink()]) : undefined,
+  link: isBrowser() ? splitLink() : undefined,
 });
 
 export default client;
