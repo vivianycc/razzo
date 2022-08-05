1091db95a601d80dcb5d83e72683b9aef01d6ebd
Yuanlin Lin <im.yuanlinLin@gmail.com>
2022-08-05 11:09:05 +0800

    add primary colors into `tailwind.config.js`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./**/*.tsx'],
-  theme: { extend: {}, },
+  theme: {
+    extend: {
+      colors: {
+        'primary-50': '#F4F0F0',
+        'primary-100': '#E3DAD9',
+        'primary-200': '#D2C4C2',
+        'primary-300': '#C1AEAB',
+        'primary-400': '#B09994',
+        'primary-500': '#9F837E',
+        'primary-600': '#8E6D67',
+        'primary-700': '#775C55',
+        'primary-800': '#604B43',
+        'primary-900': '#493932',
+      }
+    },
+  },
   plugins: [],
 };
