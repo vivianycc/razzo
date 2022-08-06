bb80a567374a7d88d4e9df1c451017a11deed442
Yuanlin Lin <im.yuanlinLin@gmail.com>
2022-08-07 00:30:25 +0800

    add basic layout of deploy new service page

--- pages/projects/[projectId]/deploy.tsx
+++ pages/projects/[projectId]/deploy.tsx
@@ -1,7 +1,7 @@
 import PageHead from '@components/PageHead';
 import { useRouter } from 'next/router';
 import { gql, useMutation, useQuery } from '@apollo/client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TopNav from '@components/TopNav';
 
 const QUERY_GIT_NAMESPACE = gql`
@@ -51,12 +51,25 @@ const CREATE_SERVICE = gql`
     }
 `;
 
+const QUERY_REPO_BRANCHES = gql`
+    query ($repoOwner: String!, $repoName: String!) {
+        gitRepoBranches(
+            repoName: $repoName,
+            repoOwner: $repoOwner,
+            provider: GITHUB
+        )
+    }
+`;
+
 function DeployNewServicePage() {
 
   const projectId = useRouter().query.projectId;
   const router = useRouter();
 
-  const [namespaceID, setNamespaceID] = useState();
+  const [namespaceID, setNamespaceID] = useState<string>();
+  const [repo, setRepo] = useState<any>();
+  const [branch, setBranch] = useState<string>();
+  const [repoKeyword, setRepoKeyword] = useState<string>('');
 
   const {
     data: gitNamespaces,
@@ -73,54 +86,128 @@ function DeployNewServicePage() {
     skip: !namespaceID
   });
 
+  const {
+    data: gitRepoBranches,
+    loading: isLoadingGitRepoBranches
+  } = useQuery(QUERY_REPO_BRANCHES, {
+    fetchPolicy: 'network-only',
+    variables: {
+      repoOwner: repo?.url.split('/')[4],
+      repoName: repo?.url.split('/')[5],
+    },
+    skip: !repo
+  });
+
   const [createService] = useMutation(CREATE_SERVICE);
+
+  async function submit() {
+    if (!repo) return;
+    try {
+      const res = await createService({
+        variables: {
+          name: repo?.url.split('/')[5],
+          template: 'GIT',
+          projectID: projectId,
+          gitProvider: 'GITHUB',
+          repoOwner: repo.url.split('/')[4],
+          repoName: repo.url.split('/')[5],
+          branchName: branch
+        }
+      });
+      const serviceId = res.data.createService._id;
+      await router.push(
+        `/projects/${projectId}/services/${serviceId}`);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    if (!namespaceID) setNamespaceID(gitNamespaces?.gitNamespaces[0].id);
+  }, [gitNamespaces?.gitNamespaces]);
 
   return <div>
     <PageHead title={projectId + ' | Razzo'}/>
     <TopNav/>
-    <div
-      className="w-screen h-screen flex justify-center
-     items-center flex-col">
-      <div className="w-96">
-
-        <div className="mt-8">
-          <p className="font-extrabold">Git Accounts</p>
-          {isLoadingGitNamespaces ? <p>Loading...</p>
-            : gitNamespaces?.gitNamespaces.map(
-              (ns: any) => <div
-                key={ns.id}
-                className="cursor-pointer"
-                onClick={() => setNamespaceID(ns.id)}>
-                {ns.name}
-              </div>)}
+    <div className="container lg:max-w-[1248px] mx-auto py-2 mb-16">
+      <p className="text-5xl mt-8 mb-12">Deploy New Service</p>
+      <div className="flex gap-6">
+        <div>
+          <div className="bg-white p-8 rounded-2xl mb-6 w-64">
+            Steps
+          </div>
         </div>
+        <div className="bg-white flex-grow p-8 rounded-2xl">
+          <div>
+            <p className="text-3xl text-primary-900">Import Git Repository</p>
+            <div className="flex my-4">
+              <select
+                value={namespaceID}
+                onChange={e => setNamespaceID(e.target.value)}
+              >
+                {gitNamespaces?.gitNamespaces.map((namespace: any) => <option
+                  key={namespace.id}
+                  value={namespace.id}
+                >
+                  {namespace.name}
+                </option>)}
+              </select>
+              <input
+                placeholder="Search Repositories"
+                value={repoKeyword}
+                onChange={e => setRepoKeyword(e.target.value)}
+              />
+            </div>
+            <div className="overflow-y-scroll h-96 my-8">
+              {isLoadingGitRepos ? <p>Loading ...</p>
+                : gitRepositories?.gitRepositories.filter(
+                  (repo: any) => repo.name.includes(repoKeyword))
+                  .map((repo: any) =>
+                    <div
+                      key={repo.id}
+                      className="cursor-pointer border border-stone-200
+                      my-4 p-4 rounded-2xl flex justify-between items-center"
+                    >
+                      <p>
+                        {repo.url.split('/')[4] + ' / ' +
+                          repo.url.split('/')[5]}
+                      </p>
+                      <button
+                        onClick={() => setRepo(repo)}
+                        className="bg-stone-700 text-white px-4 py-2 rounded-lg"
+                      >
+                        Import
+                      </button>
+                    </div>)}
+            </div>
 
-        <div className="my-8">
-          <p className="font-extrabold">Git Repositories</p>
-          <div className="overflow-y-scroll h-64">
-            {isLoadingGitRepos ? <p>Loading...</p>
-              : gitRepositories?.gitRepositories.map((repo: any) =>
-                <p
-                  key={repo.id}
-                  className="cursor-pointer"
-                  onClick={async () => {
-                    const res = await createService({
-                      variables: {
-                        name: repo.url.split('/')[5],
-                        template: 'GIT',
-                        projectID: projectId,
-                        gitProvider: 'GITHUB',
-                        repoOwner: repo.url.split('/')[4],
-                        repoName: repo.url.split('/')[5],
-                        branchName: 'main'
-                      }
-                    });
-                    const serviceId = res.data.createService._id;
-                    await router.push(
-                      `/projects/${projectId}/services/${serviceId}`);
-                  }}>
-                  {repo.name}
-                </p>)}
+            <div className="my-8">
+              <p className="font-extrabold">Git Branches</p>
+              <div className="overflow-y-scroll h-64">
+                {isLoadingGitRepoBranches
+                  ? <p>Loading ...</p> :
+                  <select
+                    value={branch}
+                    onChange={e => setBranch(e.target.value)}>
+                    {gitRepoBranches?.gitRepoBranches.map(
+                      (branchName: string) =>
+                        <option
+                          key={branchName}
+                          value={branchName}
+                          className="cursor-pointer"
+                          onClick={() => setBranch(branchName)}>
+                          {branchName}
+                        </option>)}
+                  </select>}
+              </div>
+            </div>
+
+            {(repo && branch) && <button
+              onClick={submit}
+              className="bg-stone-700 text-white px-4 py-2 rounded-lg"
+            >
+              Deploy
+            </button>}
           </div>
         </div>
       </div>
