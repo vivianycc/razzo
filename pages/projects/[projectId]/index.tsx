e090908fffcebceec2024d68a179e695547ac9ba
Vivian Chen <vivianchentl@gmail.com>
2022-08-15 18:26:10 +0800

    style project overview and create service card

--- pages/projects/[projectId]/index.tsx
+++ pages/projects/[projectId]/index.tsx
@@ -1,19 +1,20 @@
-import PageHead from '@components/PageHead';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import TopNav from '@components/TopNav';
-import { useServicesData } from '@stores/services';
-import { useProjectData } from '@stores/projects';
-import { gql, useMutation } from '@apollo/client';
+import PageHead from "@components/PageHead";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import TopNav from "@components/TopNav";
+import ServiceCard from "@components/ServiceCard";
+import { useServicesData } from "@stores/services";
+import { useProjectData } from "@stores/projects";
+import { gql, useMutation } from "@apollo/client";
+import { Button, Text } from "@geist-ui/core";
 
 const DELETE_PROJECT = gql`
-    mutation($projectId: ObjectID!) {
-        deleteProject(_id: $projectId)
-    }
+  mutation ($projectId: ObjectID!) {
+    deleteProject(_id: $projectId)
+  }
 `;
 
 function ProjectInfoPage() {
-
   const projectId = useRouter().query.projectId as string | undefined;
 
   const router = useRouter();
@@ -22,52 +23,122 @@ function ProjectInfoPage() {
 
   const [deleteProject] = useMutation(DELETE_PROJECT);
 
-  return <div>
-    <PageHead title={(project?.name || projectId) + ' | Razzo'}/>
-    <TopNav/>
-    <div
-      className="w-screen h-screen flex justify-center
-     items-center flex-col">
+  return (
+    <div>
+      <PageHead title={(project?.name || projectId) + " | Razzo"} />
+      <TopNav />
+      <div className=" h-screen container lg:max-w-[1248px] mx-auto  px-12">
+        <header className="flex justify-between items-center">
+          <Text h1>{project?.name || projectId}</Text>
 
-      <div className="my-8">
-        <p
-          className="text-red-500 cursor-pointer"
-          onClick={async () => {
-            await deleteProject({ variables: { projectId } });
-            await router.push('/projects/[projectId]',
-              `/projects/${projectId}`);
-          }}>
-          Delete Project
-        </p>
-      </div>
+          <div>
+            <Button>Project Settings</Button>
 
-      <Link
-        href="/projects/[projectId]/deploy"
-        as={`/projects/${projectId}/deploy`}
-      >
-        <a className="text-blue-500 my-8">Deploy New Service</a>
-      </Link>
+            <Link
+              href="/projects/[projectId]/deploy"
+              as={`/projects/${projectId}/deploy`}
+            >
+              <a className="text-blue-500 my-8">
+                <Button type="secondary">Deploy New Service </Button>
+              </a>
+            </Link>
+          </div>
+        </header>
 
-      <div>
-        <p>Services of this Project</p>
+        <div id="overview" className="flex gap-3">
+          <div className="box-border bg-white rounded-3xl w-1/2 p-6 drop-shadow-xl">
+            <h5 className="text-base">Service Status</h5>
+            <div className="flex justify-between">
+              <div className="flex flex-col items-center">
+                <p className="text-xs text-gray-400 font-bold">Total</p>
+                <p className="text-2xl my-0 font-bold">7</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <p className="text-xs text-gray-400 font-bold">Running</p>
+                <p className="text-2xl my-0 font-bold">6</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <p className="text-xs text-gray-400 font-bold">Crashed</p>
+                <p className="text-2xl my-0 font-bold">1</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <p className="text-xs text-gray-400 font-bold">Deploying</p>
+                <p className="text-2xl my-0 font-bold">2</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="box-border bg-primary-100 rounded-3xl w-1/2 p-6 flex justify-between drop-shadow-xl">
+            <div className="flex flex-col items-center justify-between">
+              <p className="text-sm text-gray-900 mt-0 font-bold">
+                <span>Total Cpu Usage</span>
+                <br />
+                <span className="text-xs text-gray-400">minutely vCPU</span>
+              </p>
+              <p className="text-2xl my-0 font-bold">7</p>
+            </div>
+            <div className="flex flex-col items-center justify-between">
+              <p className="text-sm text-gray-900 mt-0 font-bold">
+                <span>Memory Usage</span>
+                <br />
+                <span className="text-xs text-gray-400 ">minutely GB</span>
+              </p>
+
+              <p className="text-2xl my-0 font-bold">6</p>
+            </div>
+            <div className="flex flex-col items-center justify-between">
+              <p className="text-sm text-gray-900 mt-0 font-bold">
+                Monthly Charges
+              </p>
+              <p className="text-2xl my-0 font-bold">1</p>
+            </div>
+          </div>
+        </div>
+
         <div>
-          {services.map(s => {
-            return <div key={s._id}>
-              <Link
-                href="/projects/[projectId]/services/[serviceId]"
-                as={`/projects/${projectId}/services/${s._id}`}
-              >
-                <div>
-                  <p className="text-blue-500 mt-8">{s.name}</p>
-                  <p>{s.gitProvider}/{s.repoOwner}/{s.repoName}</p>
+          <p>Services of this Project</p>
+          <div className="grid grid-cols-3 gap-2">
+            <ServiceCard />
+            <ServiceCard />
+            <ServiceCard />
+          </div>
+          <div>
+            {services.map((s) => {
+              return (
+                <div key={s._id}>
+                  <Link
+                    href="/projects/[projectId]/services/[serviceId]"
+                    as={`/projects/${projectId}/services/${s._id}`}
+                  >
+                    <div>
+                      <p className="text-blue-500 mt-8">{s.name}</p>
+                      <p>
+                        {s.gitProvider}/{s.repoOwner}/{s.repoName}
+                      </p>
+                    </div>
+                  </Link>
                 </div>
-              </Link>
-            </div>;
-          })}
+              );
+            })}
+          </div>
+        </div>
+        <div className="my-8">
+          <p
+            className="text-red-500 cursor-pointer"
+            onClick={async () => {
+              await deleteProject({ variables: { projectId } });
+              await router.push(
+                "/projects/[projectId]",
+                `/projects/${projectId}`
+              );
+            }}
+          >
+            Delete Project
+          </p>
         </div>
       </div>
     </div>
-  </div>;
+  );
 }
 
 export default ProjectInfoPage;
---
+++ src/components/ServiceCard/index.tsx
@@ -0,0 +1,38 @@
+import { Button, Page, Text, Grid, Card } from "@geist-ui/core";
+
+export default function ServiceCard({
+  color = "pink",
+  status = "running",
+  name = "service name",
+  deployTime = "07/25 16:00",
+  gitProvider = "github",
+  deployStatus = "deploying",
+  ...props
+}) {
+  return (
+    <Card
+      shadow
+      hoverable
+      style={{ borderRadius: "16px", backgroundColor: "#FFF" }}
+    >
+      <Card.Content className="flex">
+        <div className="bg-primary-100 w-16 h-16 rounded-lg">
+          <img src="/logo-s-mono.svg" alt="" />
+        </div>
+        <div className="ml-3">
+          <span className="flex items-center gap-2 capitalize before:content-[''] before:block before:w-3 before:h-3 before:bg-green-300 before:rounded-full ">
+            {status}
+          </span>
+          <p className="mt-2 mb-1 text-lg font-bold capitalize">{name}</p>
+          <p className="my-1 text-sm"> Deployed at {deployTime}</p>
+          <p className="my-1 text-sm text-gray-400">from {gitProvider}</p>
+          {deployStatus == "deploying" ? (
+            <p className="my-1 py-1 px-2 rounded border border-orange-200 bg-orange-200/20 text-sm text-orange-400">
+              New Deploy Ongoing
+            </p>
+          ) : null}
+        </div>
+      </Card.Content>
+    </Card>
+  );
+}
