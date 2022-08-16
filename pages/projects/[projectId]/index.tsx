0e21c2a7a03b5c477a025f719fe43cd0d4dc57fd
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-08-16 09:36:07 +0800

    fix eslint warning and use real data in service list

--- pages/projects/[projectId]/index.tsx
+++ pages/projects/[projectId]/index.tsx
@@ -9,9 +9,9 @@ import { gql, useMutation } from '@apollo/client';
 import { Button, Text } from '@geist-ui/core';
 
 const DELETE_PROJECT = gql`
-  mutation ($projectId: ObjectID!) {
-    deleteProject(_id: $projectId)
-  }
+    mutation ($projectId: ObjectID!) {
+        deleteProject(_id: $projectId)
+    }
 `;
 
 function ProjectInfoPage() {
@@ -25,8 +25,8 @@ function ProjectInfoPage() {
 
   return (
     <div>
-      <PageHead title={(project?.name || projectId) + ' | Razzo'} />
-      <TopNav />
+      <PageHead title={(project?.name || projectId) + ' | Razzo'}/>
+      <TopNav/>
       <div className=" h-screen container lg:max-w-[1248px] mx-auto  px-12">
         <header className="flex justify-between items-center">
           <Text h1>{project?.name || projectId}</Text>
@@ -46,7 +46,9 @@ function ProjectInfoPage() {
         </header>
 
         <div id="overview" className="flex gap-3">
-          <div className="box-border bg-white rounded-3xl w-1/2 p-6 drop-shadow-xl">
+          <div
+            className="box-border bg-white rounded-3xl
+            w-1/2 p-6 drop-shadow-xl">
             <h5 className="text-base">Service Status</h5>
             <div className="flex justify-between">
               <div className="flex flex-col items-center">
@@ -68,11 +70,13 @@ function ProjectInfoPage() {
             </div>
           </div>
 
-          <div className="box-border bg-primary-100 rounded-3xl w-1/2 p-6 flex justify-between drop-shadow-xl">
+          <div
+            className="box-border bg-primary-100 rounded-3xl
+            w-1/2 p-6 flex justify-between drop-shadow-xl">
             <div className="flex flex-col items-center justify-between">
               <p className="text-sm text-gray-900 mt-0 font-bold">
                 <span>Total Cpu Usage</span>
-                <br />
+                <br/>
                 <span className="text-xs text-gray-400">minutely vCPU</span>
               </p>
               <p className="text-2xl my-0 font-bold">7</p>
@@ -80,7 +84,7 @@ function ProjectInfoPage() {
             <div className="flex flex-col items-center justify-between">
               <p className="text-sm text-gray-900 mt-0 font-bold">
                 <span>Memory Usage</span>
-                <br />
+                <br/>
                 <span className="text-xs text-gray-400 ">minutely GB</span>
               </p>
 
@@ -98,28 +102,14 @@ function ProjectInfoPage() {
         <div>
           <p>Services of this Project</p>
           <div className="grid grid-cols-3 gap-2">
-            <ServiceCard />
-            <ServiceCard />
-            <ServiceCard />
-          </div>
-          <div>
-            {services.map((s) => {
-              return (
-                <div key={s._id}>
-                  <Link
-                    href="/projects/[projectId]/services/[serviceId]"
-                    as={`/projects/${projectId}/services/${s._id}`}
-                  >
-                    <div>
-                      <p className="text-blue-500 mt-8">{s.name}</p>
-                      <p>
-                        {s.gitProvider}/{s.repoOwner}/{s.repoName}
-                      </p>
-                    </div>
-                  </Link>
-                </div>
-              );
-            })}
+            {services.map((s) => <div key={s._id}>
+              <Link
+                href="/projects/[projectId]/services/[serviceId]"
+                as={`/projects/${projectId}/services/${s._id}`}
+              >
+                <ServiceCard service={s}/>
+              </Link>
+            </div>)}
           </div>
         </div>
         <div className="my-8">
